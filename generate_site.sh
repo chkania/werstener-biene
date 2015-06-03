dc40505a7d961addacbaa8f442b1a2ba1d7c9077
Christoph Kania <ck@skyb.de>
2015-06-03 09:45:31 +0200

    rsync verbose

--- generate_site.sh
+++ generate_site.sh
@@ -2,4 +2,4 @@
 cd /home/skyb/werstener-biene
 git pull
 jekyll build
-rsync -r --delete /home/skyb/werstener-biene/_site/ /var/www/virtual/skyb/werstener-biene.de/
+rsync -rv --delete /home/skyb/werstener-biene/_site/ /var/www/virtual/skyb/werstener-biene.de/
