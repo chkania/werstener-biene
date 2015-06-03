6d2aaf08b463d8d3576381854d037689bd72d608
Christoph Kania <ck@skyb.de>
2015-06-03 14:50:59 +0200

    added file exclusions

--- generate_site.sh
+++ generate_site.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 cd /home/skyb/werstener-biene
+echo "Aktualisiere aus git Repository …"
 git pull
+echo "Erzeuge Seite neu …"
 jekyll build
-rsync -rctv --delete /home/skyb/werstener-biene/_site/ /var/www/virtual/skyb/werstener-biene.de/
+echo "Aktualisiere Inhalte …"
+rsync -rctv --delete --exclude nosyncfiles.txt /home/skyb/werstener-biene/_site/ /var/www/virtual/skyb/werstener-biene.de/
+echo "Fertig"
