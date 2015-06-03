ff66c7d7a211eebca3bf5cfc01c39348b208a593
Christoph Kania <ck@skyb.de>
2015-06-03 09:46:57 +0200

    preserve time

--- generate_site.sh
+++ generate_site.sh
@@ -2,4 +2,4 @@
 cd /home/skyb/werstener-biene
 git pull
 jekyll build
-rsync -rv --delete /home/skyb/werstener-biene/_site/ /var/www/virtual/skyb/werstener-biene.de/
+rsync -rtv --delete /home/skyb/werstener-biene/_site/ /var/www/virtual/skyb/werstener-biene.de/
