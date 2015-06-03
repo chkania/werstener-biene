89e4a411da55bd7a413f83f1bedb6ad8af1db458
Christoph Kania <ck@skyb.de>
2015-06-03 14:54:52 +0200

    added parameter --lsi to jekyll build

--- generate_site.sh
+++ generate_site.sh
@@ -3,7 +3,7 @@ cd /home/skyb/werstener-biene
 echo "Aktualisiere aus git Repository …"
 git pull
 echo "Erzeuge Seite neu …"
-jekyll build
+jekyll build --lsi
 echo "Aktualisiere Inhalte …"
 rsync -rctv --delete --exclude nosyncfiles.txt /home/skyb/werstener-biene/_site/ /var/www/virtual/skyb/werstener-biene.de/
 echo "Fertig"
