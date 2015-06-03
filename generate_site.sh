b67de70f60e4a7cbfb3821389ff4d2c73c060a8f
Christoph Kania <ck@skyb.de>
2015-06-03 14:55:51 +0200

    added horizontal line to output

--- generate_site.sh
+++ generate_site.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 cd /home/skyb/werstener-biene
+echo "---------------------------------"
 echo "Aktualisiere aus git Repository …"
 git pull
+echo "---------------------------------"
 echo "Erzeuge Seite neu …"
 jekyll build --lsi
+echo "---------------------------------"
 echo "Aktualisiere Inhalte …"
 rsync -rctv --delete --exclude nosyncfiles.txt /home/skyb/werstener-biene/_site/ /var/www/virtual/skyb/werstener-biene.de/
 echo "Fertig"
