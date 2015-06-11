dce6dcb19d8d4b51e31ab9753d93b6426ee55a57
Christoph Kania <ck@skyb.de>
2015-06-11 12:50:02 +0200

    dialog

--- generate_site.sh
+++ generate_site.sh
@@ -1,20 +1,53 @@
 #!/bin/bash
 cat asciibee.txt
 cd /home/skyb/werstener-biene
-echo
-echo "---------------------------------"
-echo "Aktualisiere aus git Repository …"
-git pull
-echo "---------------------------------"
-echo "Erzeuge Seite neu …"
-jekyll build --lsi
-echo "---------------------------------"
-echo "Aktualisiere lokale Inhalte …"
-rsync -rctv --delete --exclude nosyncfiles.txt /home/skyb/werstener-biene/_site/ /var/www/virtual/skyb/werstener-biene.de/
-echo "Aktualisiere S3 Inhalte …"
-cat s3copy.txt | while read line
-do
-    s3cmd --acl-public put "/home/skyb/werstener-biene/_site/$line" "s3://werstener-biene/$line"
-#s3cmd put /home/skyb/werstener-biene/_site/images/site/Slider1_progressive_90.jpg s3://werstener-biene/images/site/Slider1_progressive_90.jpg
-done
-echo "Fertig"
+
+promtp() {
+    echo "Was willst Du tun?"
+    echo "(1) git update"
+    echo "(2) build site"
+    echo "(3) deploy site"
+    echo "(4) update cdn"
+    read -p "Deine Wahl: " action
+    echo
+}
+trap prompt SIGINT
+
+while true;
+  do
+    case "$action" in
+     1)
+        echo
+        echo "---------------------------------"
+        echo "Aktualisiere aus git Repository …"
+        git pull
+        echo "---------------------------------"
+        ;;
+     2)
+        echo "---------------------------------"
+        echo "Erzeuge Seite neu …"
+        jekyll build --lsi
+        echo "---------------------------------"
+        ;;
+     3)
+        echo "---------------------------------"
+        echo "Aktualisiere lokale Inhalte …"
+        rsync -rctv --delete --exclude nosyncfiles.txt /home/skyb/werstener-biene/_site/ /var/www/virtual/skyb/werstener-biene.de/
+        echo "---------------------------------"
+        ;;
+    4)
+        echo "---------------------------------"
+        echo "Aktualisiere S3 Inhalte …"
+        cat s3copy.txt | while read line
+            s3cmd --acl-public --cf-invalidate put "/home/skyb/werstener-biene/_site/$line" "s3://werstener-biene/$line"
+            done
+        echo "Fertig"
+        echo "---------------------------------"
+        ;;
+    0)
+        echo "quit"
+        exit 1
+        ;;
+   esac
+   prompt
+  done
